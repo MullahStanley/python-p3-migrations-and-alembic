b834a5f5431707bd7357df46ab7fa12f4fa6f296
MullahStanley <miop@Miop>
2023-12-18 00:44:51 +0300

    done

---
+++ app/migrations/versions/7cc500dfda2b_empty_init.py
@@ -0,0 +1,24 @@
+"""Empty Init
+
+Revision ID: 7cc500dfda2b
+Revises: 
+Create Date: 2023-12-18 00:42:58.388089
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '7cc500dfda2b'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
